f9ea42fa35bdbaf425a0b93ee802921e8ac283cd
isaev_aa3 <isaev_aa3@nlmk.com>
2024-12-05 12:26:38 +0300

    fix route

--- restapi-nodejs/src/routes/index.js
+++ restapi-nodejs/src/routes/index.js
@@ -5,7 +5,7 @@ const productController = require("../controllers/ProductController");
 // Root/Index Routes
 router.get("/", function (req, res, next) {
   res.json({
-    message: "Доступны пути latest: /products, /result",
+    message: "Доступны пути latest2: /products, /result",
   });
 });
 
