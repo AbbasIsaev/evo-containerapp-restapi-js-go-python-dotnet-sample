1e664416a3fe319cf74c7907c0d4ebd7fc26ce38
isaev_aa3 <isaev_aa3@nlmk.com>
2024-12-05 11:55:17 +0300

    fix route

--- restapi-nodejs/src/routes/index.js
+++ restapi-nodejs/src/routes/index.js
@@ -5,7 +5,7 @@ const productController = require("../controllers/ProductController");
 // Root/Index Routes
 router.get("/", function (req, res, next) {
   res.json({
-    message: "Call the /products route to retrieve a list of products",
+    message: "Доступны: /products, /result",
   });
 });
 
